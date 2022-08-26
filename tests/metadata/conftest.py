1214f0a2b4dc1c5e7659e4b137bf9b0f49570c80
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2022-08-26 19:41:33 -0400

    move repo and db from shared.

--- tests/metadata/conftest.py
+++ tests/metadata/conftest.py
@@ -1,40 +1,9 @@
-import os
 import pytest
 from metadata.bootstrap import bootstrap
 from dfs_shared.application.uow import UnitOfWork
-from dfs_shared.domain.repository import Repository
 from dfs_shared.domain.repository import RepositoryManager
-from dfs_shared.infrastructure.json_db import JsonDatabase
-
-
-class InMemoryRepository(Repository):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.collection = set()
-
-    def _get(self, id):
-        try:
-            return next(o for o in self.collection if o.id == id)
-        except StopIteration:
-            return None
-
-    def get_by_spec(self, spec):
-        for obj in self.collection:
-            if spec.criteria(obj):
-                return obj
-        return None
-
-    def _save(self, obj):
-        self.collection.add(obj)
-
-    def _delete(self, obj):
-        self.collection = set(filter(lambda o: o != obj, self.collection))
-
-    def _update(self, obj):
-        pass
-
-    def rollback(self, obj):
-        pass
+from dfs_shared.infrastructure.inmemory_repo import InMemoryRepository
+from metadata.filesystem.infrastructure.json_db import JsonDatabase
 
 
 class InMemoryRepositoryManager(RepositoryManager):
