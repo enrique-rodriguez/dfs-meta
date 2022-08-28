19af589acdc1040254f3ea718553b17997d46e2b
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2022-08-28 12:37:52 -0400

    Use paste server.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 with open("conf.json", "r") as f:
     config = json.load(f)
 
+
 BASE_DIR = Path(__file__).resolve().parent
 
 config["basedir"] = str(Path(__file__).resolve().parent)
@@ -14,4 +15,4 @@ port = config["server"].get("port")
 
 app = get_app(config)
 
-app.run(host=host, port=port, debug=True, reloader=True)
+app.run(host=host, port=port, server="paste", debug=True, reloader=True)
